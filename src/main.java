902947e703ca3beef0b6e0b308d94779ca287cdf
KossBoii <long.truong0320@gmail.com>
2020-04-19 20:45:27 -0700

    update main.java

--- src/main.java
+++ src/main.java
@@ -1,9 +1,12 @@
 
 public class main {
 
+	private final String userName = "root";
+	private final String password = "password";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
 	}
 
 }
